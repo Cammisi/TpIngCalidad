d10eb42fed5df45b934b78758b069f3cef44621b
francoycc <francoycc@gmail.com>
2025-09-15 22:26:26 -0300

    Agregar nuevos atributos en la Clase Coordenada

--- Coordenada.java
+++ Coordenada.java
@@ -5,15 +5,24 @@ public class Coordenada {
     private double latitud;
     private double longitud;
 
+    private double altitud;       // Nuevo atributo
+    private String descripcion;   // Nuevo atributo
+
     public Coordenada(double latitud, double longitud) {
         this.latitud = latitud;
         this.longitud = longitud;
     }
 
-    public Coordenada(){
-        
+    public Coordenada(double latitud, double longitud, double altitud, String descripcion) {
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.altitud = altitud;
+        this.descripcion = descripcion;
     }
-    
+
+    public Coordenada() {
+    }
+
     public double getLatitud() {
         return latitud;
     }
@@ -29,7 +38,22 @@ public class Coordenada {
     public void setLongitud(double longitud) {
         this.longitud = longitud;
     }
-    
-    
-    
+
+    public double getAltitud() {
+        return altitud;
+    }
+
+    public void setAltitud(double altitud) {
+        this.altitud = altitud;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
+    
+
